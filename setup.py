88f190f2dd8ebd5bdca2ec3ab4a13abf383eb7d6
mwinn53 <mwinn53@gmail.com>
2018-01-14 20:00:19 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cpscoreboard',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='mwinn',
+    author_email='legitbits@protonmail.com',
+    description='Cyber Patriot Scoreboard Companion'
+)
